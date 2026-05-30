0939b495fad7fc3a37716e09c759b1186126be69
gaurav-shift <agrawalgaurav302005@gmail.com>
2026-05-30 16:02:34 +0530

    First Commit -> Basic setup for AuthService

---
+++ AuthService/src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+const { PORT } = require('./config/serverConfig');
+
+const setupAndStartServer = () => {
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+
+    
+
+    app.listen(PORT, () => {
+        console.log(`Server started on port ${PORT}`);
+    });
+};
+
+setupAndStartServer();
